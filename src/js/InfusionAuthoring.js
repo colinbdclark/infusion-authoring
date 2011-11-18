2db8ef06c91095e92e207565c154ec7033acdb0c
Colin Clark <colinbdclark@gmail.com>
2011-11-17 21:59:48 -0500

    FLUID-4528 adds a full version of Infusion and the abiltity to inject UI Options.

--- src/js/InfusionAuthoring.js
+++ src/js/InfusionAuthoring.js
@@ -55,6 +55,14 @@
             editorTabs: {
                 type: "fluid.tabs",
                 container: "{environment}.dom.editorTabs"
+            },
+            
+            injectionPanel: {
+                type: "fluid.authoring.injectionPanel",
+                container: "{environment}.dom.injectionPanel",
+                options: {
+                    environment: "{environment}" // TODO: evil
+                }
             }
         },
         
@@ -70,38 +78,85 @@
             code: ".flc-authoring-editor.code",
             markup: ".flc-authoring-editor.markup",
             style: ".flc-authoring-editor.style",
-            preview: ".flc-authoring-preview",
-            closureButton: ".flc-authoring-closure"
+            preview: ".flc-authoring-preview"
         }
     });
     
     fluid.authoring.environment.postInit = function (that) {
-        that.inject = function (type, options) {
-            var injectionSpec = fluid.authoring.injectors[type],
-                snippet = fluid.stringTemplate(injectionSpec.template, options),
-                editor = that[that.options.editorTypes[injectionSpec.type]].editor;
-            
+        that.inject = function (editorType, snippet) {
+            var editor = that[that.options.editorTypes[editorType]].editor;
             if (editor.somethingSelected()) {
                 editor.replaceSelection(snippet);
             } else {
                 editor.replaceRange(snippet, editor.getCursor());
             }
         };
-
+        
         that.updatePreview = function () {
             that.locate("preview").html(that.markupEditor.editor.getValue());
         };
     };
     
     fluid.authoring.environment.finalInit = function (that) {
-        // Bind the editor buttons.
-        that.locate("closureButton").click(function (e) {
-            that.inject("closure");
-            return false;
-        });
-        
         // Keep the markup preview pane up to date with the editor.
         that.markupEditor.editor.setOption("onChange", that.updatePreview);
+    };
+    
+    fluid.defaults("fluid.authoring.injectionPanel", {
+        gradeNames: ["fluid.rendererComponent", "autoInit"],
+        postInitFunction: "fluid.authoring.injectionPanel.postInit",
+        finalInitFunction: "fluid.authoring.injectionPanel.finalInit",
+        
+        components: {
+            injectionTabs: {
+                type: "fluid.tabs",
+                container: "{injectionPanel}.dom.tabs"
+            }
+        },
+        
+        protoTree: {
+            namespaceField: "${namespace.name}"
+        },
+        
+        model: {
+            namespace: {
+                name: "me"
+            }
+        },
+        
+        selectors: {
+            closureButton: ".flc-authoring-inject-closure",
+            uiOptionsButton: ".flc-authoring-inject-uio",
+            namespaceField: ".flc-authoring-inject-namespace-name",
+            namespaceButton: ".flc-authoring-inject-namespace",
+            tabs: ".flc-authoring-injectPanel-tabs"
+        }
+    });
+    
+    fluid.authoring.injectionPanel.postInit = function (that) {
+        that.inject = function (type) {
+            var injectionSpec = fluid.authoring.injectors[type],
+                modelVals = that.model[type],
+                snippet = fluid.stringTemplate(injectionSpec.template, modelVals);
+            
+            that.options.environment.inject(injectionSpec.type, snippet);
+        };
+    };
+    
+    fluid.authoring.injectionPanel.finalInit = function (that) {
+        // Bind each button to its injector.
+        // TODO: This is a brittle association between selector names in the component and injector names
+        // Should be refactored so that each button/injector pair is represented by a component, a bit like an "ant."
+        fluid.each(that.options.selectors, function (selector, selectorName) {
+            var buttonIdx = selectorName.indexOf("Button");
+            if (buttonIdx != -1) {
+                that.locate(selectorName).click(function (e) {
+                    var injectorName = selectorName.substring(0, buttonIdx);
+                    that.inject(injectorName);
+                    return false;
+                })
+            }
+        });
     };
     
     fluid.authoring.injectors = {
@@ -111,11 +166,51 @@
         },
         "namespace": {
             type: "js",
-            template: "    fluid.registerNamespace(\"%namespace\");"
+            template: "    fluid.registerNamespace(\"%name\");"
         },
         "component": {
             type: "js",
             template: "    fluid.defaults(\"%componentName\", %defaults);\n\n %componentName.finalInit = function (that) {\n\n};"
+        },
+        "uiOptions": {
+            type: "html",
+            template: "<link rel=\"stylesheet\" type=\"text/css\" href=\"framework/fss/css/fss-layout.css\" />" + "\n" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"framework/fss/css/fss-text.css\" />" + "\n" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"components/uiOptions/css/fss/fss-theme-bw-uio.css\" />" + "\n" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"components/uiOptions/css/fss/fss-theme-wb-uio.css\" />" + "\n" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"components/uiOptions/css/fss/fss-theme-by-uio.css\" />" + "\n" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"components/uiOptions/css/fss/fss-theme-yb-uio.css\" />" + "\n" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"components/uiOptions/css/fss/fss-text-uio.css\" />" + 
+                "\n\n" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"lib/jquery/ui/css/fl-theme-hc/hc.css\" />" + "\n" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"lib/jquery/ui/css/fl-theme-hci/hci.css\" />" + "\n" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"lib/jquery/ui/css/fl-theme-blackYellow/blackYellow.css\" />" + "\n" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"lib/jquery/ui/css/fl-theme-yellowBlack/yellowBlack.css\" />" + 
+                "\n\n" +
+                "<link rel=\"stylesheet\" type=\"text/css\" href=\"components/uiOptions/css/FatPanelUIOptions.css\" />" + 
+                "\n\n" +
+                "<!-- The Infusion Library -->" + "\n" + 
+                "<script type=\"text/javascript\" src=\"InfusionAll.js\"></script>" + 
+                "\n\n" +
+                "<div class=\"flc-uiOptions-fatPanel fl-uiOptions-fatPanel\">" + "\n" + 
+                "    <!-- This is the div that will contain the UI Options component -->" + "\n" + 
+            	"    <div id=\"myUIOptions\" class=\"flc-slidingPanel-panel flc-uiOptions-iframe\"></div>" + "\n\n" +     
+
+                "    <!-- This div is for the sliding panel that shows and hides the UI Options controls -->" + "\n" +
+            	"    <div class=\"fl-panelBar\">" + "\n" +
+            	"        <button class=\"flc-slidingPanel-toggleButton fl-toggleButton\">Display Preferences</button>" + "\n" +
+            	"    </div>" + "\n" +
+                "</div>" + "\n\n" + 
+                "<script type=\"text/javascript\">" + "\n" +
+                "    // Instantiate the UI Enhancer component, specifying the table of contents' template URL" + "\n" +
+                "    fluid.pageEnhancer({" + "\n" +
+                "        tocTemplate: \"../../lib/infusion/components/tableOfContents/html/TableOfContents.html\"" + "\n" +
+                "    });" + "\n" +
+                "    // Start up UI Options" + "\n" +
+                "    fluid.uiOptions.fatPanel(\".flc-uiOptions-fatPanel\", {" + "\n" +
+                "        prefix: \"../../lib/infusion/components/uiOptions/html/\"" + "\n" +
+                "    });" + "\n" +
+                "</script>"
         }
     };
         
